388020a1d4d489465bb46f24229d027d0ef34d2d
saepul anwar <adm.ipul@gmail.com>
2026-08-15 11:46:31 +0700

    Updates

--- _next/static/chunks/app/articles/[slug]/page-bc903f6bd68a367a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[207,904],{8311:(e,s,n)=>{Promise.resolve().then(n.bind(n,8452)),Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[619,356,452,441,255,358],()=>e(e.s=8311)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/products/page-0edcad5d36f19095.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[571],{3672:(e,s,n)=>{Promise.resolve().then(n.bind(n,8452)),Promise.resolve().then(n.t.bind(n,2619,23))}},e=>{e.O(0,[619,452,441,255,358],()=>e(e.s=3672)),_N_E=e.O()}]);
